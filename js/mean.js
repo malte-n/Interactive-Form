3c73373d7832b72a769a891080a581e81b71145c
Malte Niepel <maltetorben.niepel@gmail.com>
2016-05-15 14:28:31 +0200

    necessary requirements met

--- js/mean.js
+++ js/mean.js
@@ -1,10 +1,9 @@
-
 $('#colors-js-puns').hide();
-
+$("#credit-card").hide().siblings().not("#payment").hide().not("#payment").parent().children().first().show();
 //Make sure you add an text input field.
 var optionSelected;
-$('#title').on("change", function() {
-  optionSelected = $(this).val();
+$('#title[value="other"').on("change", function() {
+  optionSelected = $("#title").val();
   console.log(optionSelected);
   var otherTitleField = '<input type="text" id="other-title" placeholder="Your Title">';
   if (optionSelected === "other"){
@@ -15,7 +14,6 @@ $('#title').on("change", function() {
   //Add placeholder text of "Your Title" for the field
   }
 });
-var otherTitle = $("#other-title").val();
 
 var designSelected = "Select Theme";
 $('#design').on("change", function() {
@@ -69,7 +67,7 @@ $(".activities [type='checkbox']").on("change", function(){
         checkCount--;
         //When a user unchecks an activity competing activities are no longer disabled.
         $(".activities [type='checkbox'][data-timeslot=" + group + "]")
-        .not(this).removeProp('disabled').parent().css({color: ''});;
+        .not(this).removeProp('disabled').parent().css({color: ''});
       }
     });
   } else if (this === allCheckbox && $(this).is(":checked")) {
@@ -79,7 +77,7 @@ $(".activities [type='checkbox']").on("change", function(){
   }
   if (checkCount === 0) {
     $(".total-cost").remove();
-    return false
+    return false;
   }
   addedAmount = checkCount*100;
   totalCost = '<div class="total-cost">Total: $' + addedAmount + '</div>';
@@ -89,45 +87,25 @@ $(".activities [type='checkbox']").on("change", function(){
 
 //When a user selects the "Credit Card" payment option, display the #credit-card div, and hide the "Paypal" and "Bitcoin information.
 var paymentOptionSelected = "select_method";
-
-function myFunction(paymentOption) {
-  $("#credit-card").hide().siblings().not("#payment").hide();
+function myFunction() {
+  $("#credit-card").hide().siblings().not("#payment").hide().not("#payment").parent().children().first().show();
   console.log("got executed");
   $("#payment").on('change', function(){
     paymentOptionSelected = $(this).val();
     console.log(paymentOptionSelected);
     if (paymentOptionSelected === "credit card") {
-      $("#credit-card").show().siblings().not("#payment").hide();
+      $("#credit-card").show().siblings().not("#payment").hide().not("#payment").parent().children().first().show();
     } else if (paymentOptionSelected === "paypal") {
-      $("#credit-card").siblings().eq(3).show().siblings().not("#payment").hide();
+      $("#credit-card").siblings().eq(3).show().siblings().not("#payment").hide().not("#payment").parent().children().first().show();
     } else if (paymentOptionSelected === "bitcoin") {
-      $("#credit-card").siblings().eq(4).show().siblings().not("#payment").hide();
+      $("#credit-card").siblings().eq(4).show().siblings().not("#payment").hide().not("#payment").parent().children().first().show();
     } else {
-      $("#credit-card").hide().siblings().not("#payment").hide();
+      $("#credit-card").hide().siblings().not("#payment").hide().not("#payment").parent().children().first().show();
     }
   });
 }myFunction();
 
 var validateInputs = function() {
-  //input name check
-  if ($("input#name").val() == ''){
-    console.log();
-    $("input#name").prev().text("Name: (please provide your name)").css("color", "#ec2b45");
-  } else {
-    $("input#name").prev().text("Name:").css({'color': ''});
-  }
-  //input email check
-  if ($("input#mail").val() == '') {
-    $("input#mail").prev().text("Email: (please provide your email)").css("color", "#ec2b45");
-  } else {
-    $("input#mail").prev().text("Name:").css({'color': ''});
-  }
-  //select a job role check
-  if (optionSelected === "other" && $("#other-title").val() == '') {
-    $("#title").prev().text("Job Role: (please provide your job role)").css("color", "#ec2b45");
-  } else {
-    $("#title").prev().text("Job Role:").css({'color': ''});
-  }
   //select a t-shirt check
   if (designSelected === "Select Theme") {
     $(".shirt legend").after('<label class="shirtLabel" style="color: #ec2b45">Don&#8217;t forget to pick a T-Shirt</label>');
@@ -140,24 +118,57 @@ var validateInputs = function() {
   } else {
     $(".activitiesLabel").remove();
   }
-}
+  if (paymentOptionSelected == "select_method") {
+    $("#payment").siblings().eq(0).after('<label class="paymentLabel" style="color: #ec2b45">Please select a payment option</label>');
+    console.log("Payment option reminder");
+  } else {
+    $(".paymentLabel").remove();
+    console.log("Payment option reminder removed");
+  }
+  if ( paymentOptionSelected === "credit card" && ($("#cc-num").val() === ''|| $("#zip").val() === '' || ($("#cvv").val() === '' && $("#cvv").val().length === 3) || $("#exp-month").val() === '')) {
+    $("#payment").siblings().eq(0).after('<label class="ccLabel" style="color: #ec2b45">Please fill out your credit-card credentials correctly</label>');
+  } else {
+    $(".ccLabel").remove();
+  }
+};
+
+
+
+var validateTextInputs = function() {
+  //input name check
+  if ($("input#name").val() === ''){
+    $("input#name").prev().text("Name: (please provide your name)").css("color", "#ec2b45");
+  } else {
+    $("input#name").prev().text("Name:").css({'color': ''});
+  }
+  //input email check
+  var email = $("input#mail").val();
+  function validateEmail(email) {
+    var emailRegEx = /\S+@\S+\.\S+/;
+    return emailRegEx.test(email);
+  }
+  if (validateEmail(email) === true && email !== '') {
+    $("input#mail").prev().text("Email:").css({'color': ''});
+  } else {
+    $("input#mail").prev().text("Email: (please provide your email)").css("color", "#ec2b45");
+  }
+  //select a job role check
+  if (optionSelected === "other" && $("#other-title").val() === '') {
+    $("#title").prev().text("Job Role: (please provide your job role)").css("color", "#ec2b45");
+  } else {
+    $("#title").prev().text("Job Role:").css({'color': ''});
+  }
+};
 
 //register button calls validateInputs
 $("button[type='submit']").on("click", function(e){
     e.preventDefault();
     //Name field can't be empty
-    console.log("Click registered.");
-    $(".shirtLabel").remove();
-    $(".activitiesLabel").remove();
+    $(".shirtLabel, .activitiesLabel, .paymentLabel, .ccLabel").remove();
+    //$(".paymentLabel").remove();
     validateInputs();
-    //if the user writes into the forms input, check again with validateInputs
-    $("form input").on("keyup", function() {
-      console.log("executed");
-      validateInputs();
-    });
+    validateTextInputs();
 });
-
-
 
 
 
